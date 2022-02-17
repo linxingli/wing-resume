453b511d296e2ac2619002c73e43bb0a2f609cd7
linxingli <1531780174@qq.com>
2022-02-17 20:21:28 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Resume from './resume/index.js';
+
+// function App () {
+//   return (
+//     <Resume />
+//   );
+// }
+
+class App extends Component {
+  render () {
+    return (
+      <Resume />
+    );
+  }
+}
+
+export default App;
