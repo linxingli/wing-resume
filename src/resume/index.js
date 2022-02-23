c81ed6226cde694c47e4149e204c75dfbf83bab0
linxingli <1531780174@qq.com>
2022-02-23 21:57:36 +0800

    add print css

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,16 +1,7 @@
 import Markdown from 'markdown-to-jsx';
 import React from 'react';
-// import { render } from 'react-dom';
 import './index.css';
 import lxl from './lxl.js'
-
-// class Resume extends Component {
-//     render () {
-//         return (
-//             <Markdown children={md}></Markdown>
-//         )
-//     }
-// }
 
 function Resume () {
     return (
